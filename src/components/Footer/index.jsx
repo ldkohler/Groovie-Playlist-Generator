d540497d2f1321f4e1b74e7837b5fe665fb7848a
Lucas Kohler <lucasdkohler@gmail.com>
2023-07-06 01:17:25 -0400

    temp footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-icons">
+        <p id="footer-p">
+          Send me an email at{" "}
+          <a href="mailto:zeq5dh@virginia.edu">zeq5dh@virginia.edu</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
