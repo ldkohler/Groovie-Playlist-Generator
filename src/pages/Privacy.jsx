302e356176d1b148cd014938280785552723bd09
Lucas Kohler <lucasdkohler@gmail.com>
2023-07-14 19:28:21 -0400

    privacy policy added

---
+++ src/pages/Privacy.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Privacy() {
+        return (
+          <div className="page-wrapper">
+            <div>
+            <h1><strong>Privacy Policy</strong></h1>
+            <p>
+            Spotify was developed as an open source app powered by the Spotify Web API. 
+            By choosing to use this app, you agree to the use of your Spotify account username and data for 
+            your top artists and tracks.
+            </p>
+
+            <p>
+            None of the data used by Receiptify is stored or collected anywhere, and it is NOT shared with 
+            any third parties. All information is used solely for generating your playlist.
+            </p>
+      
+            </div>
+          </div>
+        );
+      }
+      
+  
+  export default Privacy;
