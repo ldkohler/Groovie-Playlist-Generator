7407a67bc84ce5b8b4393b9acfa2db30e4079b63
Lucas Kohler <lucasdkohler@gmail.com>
2023-07-23 21:08:28 -0400

    name update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,26 +7,26 @@ function About() {
             <div>
             <h1><strong>About</strong></h1>
             <p>
-            SpotiFilm is a <em>dynamic playlist generator</em> that curates <em>personalized Spotify playlists</em> inspired 
+            Groovie is a <em>dynamic playlist generator</em> that curates <em>personalized Spotify playlists</em> inspired 
             by your chosen movie genre and your unique taste. Tailoring a playlist is an
               effortless process, and the final playlist is seamlessly integrated into your Spotify
                account.
             </p>
 
             <p>
-            Each SpotiFilm playlist is one of a kind, expertly woven with your chosen cinematic vibes
+            Each Groovie playlist is one of a kind, expertly woven with your chosen cinematic vibes
              and individual preferences to choose the tracks. Whether you're a fan of high-octane action
               movies, classic romance films, or eerie horror flicks, your ideal playlist is waiting for
-               you here on SpotiFilm. Experience your favorite genres in a whole new light, through the
+               you here on Groovie. Experience your favorite genres in a whole new light, through the
                 power of music.
             </p>
       
-            <p id="end">Feel free to reach out to me to get in touch!</p>
+            <p className="pb-8">Feel free to reach out to me to get in touch!</p>
             </div>
 
             <div>
-                <Link to='/' className='btn btn-outline btn-spotify rounded-full w-full text-base font-noto-sans'>
-                CREATE YOUR SPOTIFILM PLAYLIST 
+                <Link to='/' className='btn btn-lg btn-outline btn-spotify rounded-full w-about text-xl font-noto-sans'>
+                CREATE YOUR GROOVIE PLAYLIST 
                 </Link>
             </div>
           </div>
